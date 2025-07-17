3695831307f6e5140abebf4e3de4bd3d895219b6
Pedro José C.A <pedrojos.campos@protonmail.com>
2025-07-17 18:22:52 -0300

    feat: uses integration of pycaret and mlflow to auto train a dataset

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print("Testando")
+import pandas as pd
+import mlflow
+
+from pycaret.regression import RegressionExperiment
+
+mlflow.set_tracking_uri(uri='http://localhost:8222')
+
+dataset = pd.read_csv('boston_weather_data.csv')
+
+pycaret_experiment = RegressionExperiment()
+pycaret_experiment.setup(data=dataset, target='tmax', log_experiment=True)
+
+best_regressors = pycaret_experiment.compare_models()
