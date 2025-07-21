6c6571af56be803085c258d5cfe41a4258fdb1f2
Pedro José C.A <pedrojos.campos@protonmail.com>
2025-07-21 12:39:37 -0300

    feat: create page with streamlit to upload files

--- main.py
+++ main.py
@@ -1,13 +1,22 @@
 import pandas as pd
+import streamlit as st
 import mlflow
-
 from pycaret.regression import RegressionExperiment
 
 mlflow.set_tracking_uri(uri='http://localhost:8222')
 
-dataset = pd.read_csv('boston_weather_data.csv')
+st.title("AutoTrain to MLFlow")
+uploaded_file = st.file_uploader("Choose a dataset", type="csv")
 
-pycaret_experiment = RegressionExperiment()
-pycaret_experiment.setup(data=dataset, target='tmax', log_experiment=True)
+if uploaded_file is not None:
+    try:
+        dataset_streamlit = pd.read_csv(uploaded_file)
+        st.dataframe(dataset_streamlit.head())
+        pycaret_experiment = RegressionExperiment()
+        pycaret_experiment.setup(data=dataset_streamlit, target='tmax', log_experiment=True)
 
-best_regressors = pycaret_experiment.compare_models()
+        best_regressors = pycaret_experiment.compare_models()
+    except Exception as e:
+        st.error(f"Ocorreu um erro ao ler o arquivo: {e}")
+else:
+    st.info("Por favor, faça o upload de um arquivo CSV para continuar.")
