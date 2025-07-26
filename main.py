02314388e341ee4af782852925a89be589257c66
Pedro José C.A <pedrojos.campos@protonmail.com>
2025-07-26 11:43:22 -0300

    feat: allow target feature selection

--- main.py
+++ main.py
@@ -3,19 +3,35 @@ import streamlit as st
 import mlflow
 from pycaret.regression import RegressionExperiment
 
+st.title("AutoTreinamento no MLFlow")
+
 mlflow.set_tracking_uri(uri='http://localhost:8222')
 
-st.title("AutoTrain to MLFlow")
-uploaded_file = st.file_uploader("Choose a dataset", type="csv")
+uploaded_file = st.file_uploader("Escolha seu dataset csv", type="csv")
+target_feature = None
+clicked = False
+
+def register_best_model():
+    print()
+
+
+def auto_train():
+    pycaret_experiment = RegressionExperiment()
+    pycaret_experiment.setup(data=dataset_streamlit, target=target_feature, log_experiment=True)
+    best_model = pycaret_experiment.compare_models()
+    st.success("Treinamento concluído e modelos logados no MLflow!")
+    st.write("Melhor modelo encontrado:")
+    st.write(best_model)
 
 if uploaded_file is not None:
     try:
         dataset_streamlit = pd.read_csv(uploaded_file)
-        st.dataframe(dataset_streamlit.head())
-        pycaret_experiment = RegressionExperiment()
-        pycaret_experiment.setup(data=dataset_streamlit, target='tmax', log_experiment=True)
+        st.dataframe(dataset_streamlit.head()) 
+        target_feature = st.selectbox(label="Selecione o atributo meta", options=dataset_streamlit.columns)
+        clicked = st.button("Treinar")
+        if clicked and target_feature is not None:
+            auto_train()
 
-        best_regressors = pycaret_experiment.compare_models()
     except Exception as e:
         st.error(f"Ocorreu um erro ao ler o arquivo: {e}")
 else:
